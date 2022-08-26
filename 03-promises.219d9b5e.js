841ecd373758e71389b0eebd4930a9afd2b9f56b
VadimLuginets <99825972+VadimLuginets@users.noreply.github.com>
2022-08-26 20:40:33 +0000

    Deploying to gh-pages from @ VadimLuginets/goit-js-hw-09@b3d3db7d556a4528b4e1613d82498b3766dc1942 🚀

--- 03-promises.219d9b5e.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("form");let t=0,o=0;e.addEventListener("submit",(l=>{l.preventDefault(),setTimeout((()=>{t=Number(e.step.value);for(let l=1;l<=e.amount.value;l++)o+=t,i(l,o).then((({position:e,delay:t})=>{alert(`Fulfilled promise ${e} in ${t} ms`)})).catch((({position:e,delay:t})=>{alert(`Rejected promise ${e} in ${t} ms`)}))}),Number(e.delay.value))}));const i=(e,t)=>new Promise(((o,i)=>{const l=Math.random()>.3;setTimeout((()=>{l?o({position:e,delay:t}):i({position:e,delay:t})}),t)}));
-//# sourceMappingURL=03-promises.219d9b5e.js.map
